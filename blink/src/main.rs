e6d94d4103fd84d593cb0a9bbd73894d50ea399c
kromych <kromych@users.noreply.github.com>
2023-09-09 21:39:07 -0700

    Blinks

--- blink/src/main.rs
+++ blink/src/main.rs
@@ -8,24 +8,27 @@ use panic_halt as _;
 
 use embedded_hal::prelude::*;
 use embedded_hal::watchdog::WatchdogDisable;
+use slstk3400a::leds::LedTrait;
 use slstk3400a::SlStk3400a;
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
     let mut board = SlStk3400a::new().unwrap();
+    let leds: [&mut dyn LedTrait; 2] = [&mut board.leds.led0, &mut board.leds.led1];
 
     // If the Watchdog is not reset/disabled, the board will reboot.
     board.watchdog.disable();
 
-    let mut count = 0u32;
-    while count < 1000 {
+    let mut count = 0usize;
+    loop {
+        leds[count & 1].on();
+        board.delay.delay_ms(1000u16);
+
         defmt::info!("Hello, world #{}!", count);
 
-        board.delay.delay_ms(500u16);
-        count = count.wrapping_add(1);
-    }
+        leds[count & 1].off();
+        board.delay.delay_ms(1000u16);
 
-    loop {
-        cortex_m::asm::wfe();
+        count = count.wrapping_add(1);
     }
 }
